921576e1e40d0b47a2e490551bbbab6d8d06c50d
Sergey Kruk <sergey.kruk@gmail.com>
2013-10-23 17:17:52 +0400

    added check-create note file if missing on read or write, removed extra whitespaces and file offset

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ var express = require("express");
 var server = express();
 var fs = require("fs");
 var notePath = __dirname + "/notes/note.txt";
+var noteDir = __dirname + "/notes";
+
+function checkNoteFile() {
+	if (!fs.existsSync(notePath)) {
+		if (!fs.existsSync(noteDir)) {
+			fs.mkdirSync(noteDir);
+		}
+		fs.openSync(notePath, "w");
+	}
+}
 
 server.set("view engine", "ejs");
 server.set("view options", {layout: true, root: __dirname + "/views"});
@@ -10,14 +20,16 @@ server.use(express.static(__dirname + "/public"));
 server.use(express.bodyParser());
 
 server.get("/", function(req, res){
+	checkNoteFile();
 	res.render("index", {content: fs.readFileSync(notePath)});
 });
 
 server.post("/save", function(req, res){
+	checkNoteFile();
 	var obj = req.body.note;
 	var noteFile = fs.openSync(notePath, 'w');
 	var buf = new Buffer(obj.toString());
-	fs.writeSync(noteFile, buf, 0, buf.length, 5);
+	fs.writeSync(noteFile, buf, 0, buf.length);
 	console.log(JSON.stringify(req.body));
 	res.send("ok");
 })
